84a015b400ec79202ddaffcfbbac465d5286c667
Adrian Biagioli <adrianbiagioli@gmail.com>
2019-04-29 15:42:20 -0400

    ditto

--- renderer/CudaScene.h
+++ renderer/CudaScene.h
@@ -1,8 +1,12 @@
 #ifndef RAYMARCHER_CUDASCENE_H
 #define RAYMARCHER_CUDASCENE_H
 
+#if defined(__CUDACC__)
 #include <cuda.h>
 #include <cuda_runtime.h>
+#define GLM_FORCE_CUDA
+#endif
+
 #include <vector>
 
 #include "CudaPrimitive.h"
@@ -19,8 +23,9 @@ private:
     CudaSphere *cudaDeviceSphereData;
 };
 
+#if defined(__CUDACC__)
 __device__ float deviceSdf(glm::vec3 p);
 __device__ glm::vec3 deviceNormal(glm::vec3 p);
-
+#endif
 
 #endif //RAYMARCHER_CUDASCENE_H
